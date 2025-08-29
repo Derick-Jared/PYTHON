3ac1f387fd3031288dbbd95a758c3128544ad145
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2025-08-28 23:39:48 -0500

    Initial commit

---
+++ ejercicios.py
@@ -0,0 +1,9 @@
+tam = input("Ingrese el tamaño de la lista: ")
+digitos = []
+for i in range(int(tam)):
+    digito=input("Ingrese el digito: ")
+    digitos.append(digito)
+
+print("La lista es:")
+print(digitos)
+    
---
+++ prueba.py
@@ -0,0 +1,75 @@
+# print("Hola mundo")
+
+# nombre = input("Como te llamas?")
+
+# print("Tu nombre es: ", nombre)
+
+# edad = int(input("Cuantos a;os tienes?"))
+
+# if edad>=18:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+
+# name = "Mi nombre es Darvin"
+# print(name)
+# nametitle = name.title()
+# nameupper = name.upper()
+# namelower = name.lower()
+# print(nametitle)
+# print(nameupper)
+# print(namelower)
+
+# name2 = "Darvin"
+
+# name2 +=" Lopez"
+
+# print(name2)
+# print("\t"+name2)
+
+# prueba = "  Hola mundo   "
+# print(prueba)
+# print(prueba.rstrip())
+
+#==============================LISTAS=================================
+# nombres = ["Deck","Dirick","Darvin"]
+# print(nombres)
+# print(nombres[2])
+# nombres.append("Ana")
+# print(nombres)
+
+# nombres2 = []
+# nombres2.append("Maria")
+# nombres2.append("Juana")
+# print(nombres2)
+# juanita = nombres2.pop(1)
+# print(juanita)
+# print(nombres2)
+# nombres2.remove("Maria")
+# print(nombres2)
+# print(sorted(nombres))
+# nombres.sort()
+# print(nombres)
+# nombres.sort(reverse=True)
+# print(nombres)
+# nombres.reverse()
+# print(nombres)
+
+#==============================TRABAJANDO CON LISTAS=================================
+# nombres = ["Deck","Dirick","Darvin"]
+# for nombre in nombres:
+#     print(nombre)
+
+# for i in range(len(nombres)):
+#     print(nombres[i])
+    
+# for i in range(5,15):
+#     print(i)
+
+digitos = list(range(10))
+minimo = min(digitos)
+maximo = max(digitos)
+suma = sum(digitos)
+print(minimo)
+print(maximo)
+print(suma)
