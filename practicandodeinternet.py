cb381ae9a5c8294076f752f23ab3f57f9e242419
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2025-08-29 00:12:48 -0500

    feat: add ejercicio del 1 al 7
    Repaso python

---
+++ practicandodeinternet.py
@@ -0,0 +1,44 @@
+#https://www.studocu.com/latam/document/universidad-nacional-experimental-de-los-llanos-occidentales-ezequiel-zamora/informatica/400-ejercicios-de-python-desde-nivel-basico-a-avanzado-con-aplicaciones-en-i/82255584
+#1) Hacer un programa donde se pida un nombre por teclado y se imprima “Hola ..el_nombre_ingresado”
+# name = input("Ingrese tu nombre: ")
+# print("Hola tu nombre es :"+name)
+#2) Hacer un programa que solicite por teclado dos número y muestre la suma ,
+#la resta ,la multiplicación y la división de esos números 
+# number1 = float(input("Ingrese el primer numero: "))
+# number2 = float(input("Ingrese el segundo numero: "))
+# suma = number1 + number2
+# resta = number1 - number2
+# multiplicacion = number1 * number2
+# division = number1 / number2
+# print("La suma es: "+str(suma))
+# print("La resta es: "+str(resta))
+# print("La multiplicacion es: "+str(multiplicacion))
+# print("La division es: "+str(division))
+#3) Hacer un programa que solicite una edad y determine si es mayor de edad. 
+#4) Hacer un programa que solicite una edad e imprima “Es mayor” si es mayor de edad ,
+# sino que imprima “Es menor”   
+# years = int(input("Ingrese su edad: "))
+# if years >= 18:
+#     print("Usted es mayor de dedad")
+# else:
+#     print("Usted es menor de edad")
+#5) Hacer un programa que solicite un color por teclado e imprima
+# “Puede pasar “ si el color ingresado es verde , “Precaución “ si es amarillo ,
+# “No pasar “ si es rojo o “Color inválido” si es cualquier otro.
+# color = input("Ingrese un color: ")
+# if color.lower() == "verde":
+#     print("Puede pasar")
+# elif color.lower() == "amarillo":
+#     print("Precaucion")
+# elif color.lower() == "rojo":
+#     print("No pasar")
+# else:
+#     print("Color invalido")
+#6) Hacer un programa que cuente desde el 1 al 100 y los imprima uno a uno. 
+# for i in range(1,101):
+#     print(i)
+#7) Hacer un programa que cuente del 1 al 100 inclusive e imprima sólo los números pares
+# for i in range(1,101):
+#     if i % 2 == 0:
+#         print(i)
+
