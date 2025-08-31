6f2a5edf2a242c8ffb94e800db64e66900aed9f9
Derick-Jared <108320862+Derick-Jared@users.noreply.github.com>
2025-08-31 13:53:36 -0500

    feat: add 16 to 22

--- practicandodeinternet.py
+++ practicandodeinternet.py
@@ -115,3 +115,108 @@
 # number1 = input("Ingrese el primer numero:")
 # number2 = input("Ingrese el segundo numero:")
 # EsMultiplo(int(number1),int(number2))
+#16) Crea una funcion que calcule la temperatura media de un dia a partir de la temperatura
+# maxima y minima.
+#Crear un programa principal, que utilizando la funcion anterior, vaya pidiendo
+#la temperatura maxima y minima de cada dia y vaya mostrando la media.
+#El programa pedira el numero de dias que se van a introducir.
+# def temperaturaMedia(min , max):
+#     return (min + max)/2
+
+# cantDias = int(input("Ingrese la cantidad de dias: "))
+# for i in range (cantDias):
+#     tempmin = float(input("Ingrese la temperatura minima del dia ("+str(i+1)+") :"))
+#     tempmax = float(input("Ingrese la temperatura maxima del dia ("+str(i+1)+") :"))
+#     print("La temperatura media del dia (",(i+1),") es: ",temperaturaMedia(tempmin,tempmax))
+#17)Crea una funcion "ConvertirEspaciado",  que reciba como parametro un texto y devuelve
+#una cadeena con un espacio adicional tras cada letra.
+#Ejemplo: "hola, tu" devolvera "h o l a ,   t u ". Crea un programa principal donde se use dicha funcion
+# def ConvertirEspaciado(text):
+#     new_text = ""
+#     for i in range(text.__len__()):
+#         new_text += text[i]+" "
+#     return new_text
+# text= input("Ingrese un texto: ")
+# print("El nuevo texto es: ",ConvertirEspaciado(text))
+#18)Crea una funcion "calcularMaxMin" que reciba una lista de numeros y devuelva el valor 
+#maximo y minimo. Crea un programa que pida numeros por teclado y muestre el maximo y minimo,
+#utilizando la funcion anterior.
+# def calcularMaxMin(lista):
+#     min=9999999
+#     max=-9999999
+#     for i in range (lista.__len__()):
+#         if lista[i] < min:
+#             min = lista[i]
+#         if lista[i] > max:
+#             max = lista[i]
+#     return min,max
+# numeros = []
+# cant= int (input("Ingrese la cantidad de numeros a introducir:"))
+# for i in range(cant):
+#     num = int(input("Ingrese el numero ("+str(i+1)+"): "))
+#     numeros.append(num)
+# print("El minimo y maximo son :",calcularMaxMin(numeros))
+#19)Dise;ar una funcion que calcule el area y el perimetro de una circunferencia.
+#utiliza dicha funcion en un programa principal que lea el radio de una circunferencia
+#y muestre su area y su perimetro.
+# import math
+# def calcularAreaPerimetro(radio):
+#     area=math.pi*radio**2
+#     area=round(area,2)
+#     perimetro=2*math.pi*radio
+#     perimetro=round(perimetro,2)
+#     return area,perimetro
+# radio = float (input("Ingrese el radio de la circunferencia:"))
+# print("El area y perimetro son:",calcularAreaPerimetro(radio),)
+#20)Crear una subrutina llamada "Login", que reciba un nombre de usuario y una contrase;a
+#y devuelve Verdadero si el nombre de usuario es "usuario1"
+#y la contrase;a es "asdasd". Ademas recibe el numero de intentos que se ha intentado
+#hacer login y si no se ha podido hacer login incremente el valor.
+#Crear un programa principal que pida un nombre de usuario y una contrase;a y se intente hacer
+#login, solamente tres oportunidades para intentarlo.
+# def Login(user,passw):
+#     if user == "usuario1" and passw == "asdasd":
+#         return True
+#     else:
+#         return False
+# intentos = 1
+# while (intentos<=3):
+#     user = input("Ingrese le nombre de usuario: ")
+#     passw = input("Ingrese la contrase;a: ")
+#     if Login(user,passw):
+#         print("Login exitoso")
+#         break
+#     else:
+#         print("Login fallido")
+#         print("Intento numero: ",intentos)
+#         intentos += 1
+#21)Crear una funcion recursiva que calcule el factorial de un numero. Realiza un
+#programa principal que donde se lea un entero y se muestre el resultado del facotorial
+# def factorial (number):
+#     if(number==0):
+#         return 1
+#     else:
+#         return number * factorial(number-1)
+# num = int(input("Ingrese el numero para calcular su facotrial: "))
+# print("El factorial de ",num," es: ",factorial(num))
+#22)Crear una funcion que calcule el MCD de dos numeros por el metodo Euclides.
+# el metodo euclides es el siguiente:
+# Se divide el numero mayor entre el menor
+# si la division es exacta, el divisor es el MCD
+# si la division no es exacta, dividimos entre el resto obtenido y se continua
+# de esta forma hasta obtener una division exacta, siendo el ultimo divisor el MCD
+#Crea un programa principal que que lea dos numeros y muestre el MCD
+# def MCD(num1,num2):
+#     if(num1>num2):
+#         mayor=num1
+#         menor=num2
+#     if(num2>num1):
+#         mayor=num2
+#         menor=num1
+#     if(num1%num2==0):
+#         return num1/num2
+#     else:
+#         resto=mayor%menor
+#         return MCD(menor,resto)
+# number1 = int(input("Ingrese el primer numero: "))
+# number2 = int(input("Ingrese el segundo numero: "))
